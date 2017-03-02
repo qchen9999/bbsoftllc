68a9aeff5af973fd758bed374e34a733207b8777
qchen9999 <qchen9999@yahoo.com>
2017-03-02 16:57:59 -0500

    Initial check in.

---
+++ FWNotify/FWNotify.Droid/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace FWNotify.Droid
+{
+    using Microsoft.Azure.Mobile;
+
+    [Activity(Label = "FWNotify.Droid", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            MobileCenter.Configure("2e51d45f-3694-44a9-b266-93951a218ba7");
+            LoadApplication(new App());
+        }
+    }
+}
+
---
+++ FWNotify/FWNotify/App.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+
+namespace FWNotify
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+                         new Label {
+                             HorizontalTextAlignment = TextAlignment.Center,
+                             Text = "Welcome to Xamarin Forms!"
+                         }
+                     }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            MobileCenter.Start(typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
